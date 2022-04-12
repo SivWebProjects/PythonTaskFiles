b95d0456b52f6600d0bd86242ce5e5fd8c6401b6
SivWebProjects <99475439+SivWebProjects@users.noreply.github.com>
2022-04-12 11:01:39 +0530

    Update zipping_list.py

--- python_task_programs/zipping_list.py
+++ python_task_programs/zipping_list.py
@@ -1,11 +1,11 @@
-# create four list containing string and combine every element from each list in a single tuple
-# example for two list  l1 =[1,2,3,4] l2 = [5,6,7,8]
-# ans  =[(1,5),(2,6),(3,7),(4,8)] without using for loop
+# Create four list containing string and combine every element from each list in a single tuple.
+# Example for two list  l1 =[1,2,3,4] l2 = [5,6,7,8]
+# answer  =[(1,5),(2,6),(3,7),(4,8)] without using for loop
 
-list1 =['six', 'two', 'three'] 
-list2 = ['ball', 'pen', 'pencil']
-list3 = ['ha', 'gh', 'rt', 'bn']
-list4 = ['bhn', 'hfg', 'fhy', 'ase']
+list1 = input("Enter space separated strings: ").split()
+list2 = input("Enter space separated strings: ").split()
+list3 = input("Enter space separated strings: ").split()
+list4 = input("Enter space separated strings: ").split()
 
-# combining every element from each list
+# Combining every element from each list upto equal length of all lists
 print(list(zip(list1, list2, list3, list4)))
