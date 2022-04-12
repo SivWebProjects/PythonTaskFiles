2c1a5c80e8c5d76685c6067d805934b6afd633e1
SivWebProjects <99475439+SivWebProjects@users.noreply.github.com>
2022-04-12 10:56:22 +0530

    Update reading_text_file.py

--- python_task_programs/reading_text_file.py
+++ python_task_programs/reading_text_file.py
@@ -1,35 +1,40 @@
-# Read a text file ..try to find vowels and count it 
+# Read a text file .. try to find vowels and count it.
 # Next in a dictionary put word as a key and number of times that word has occurred 
-# in that text file as a value of that key
+# in that text file as a value of that key.
 
 import re
 
 # Open the text file in read mode
-file = open("/home/neosoft/Documents/PythonFiles/venv/sample.txt", "r")
+file = open("/home/neosoft/Documents/PythonFiles/venv/text.txt", "r")
+
 text = file.read()
 text = text.lower()
 
 vowel_count = 0
 vowels_list = ['a', 'e', 'i', 'o', 'u']
 
+# Checking if the character is already in the list or not
+# If present increment the vowel count by 1
 for char in text:
-    # Check if the character is already in list
     if char in vowels_list:
         vowel_count += 1
+        
 print("Total number of vowels in a text file are: "+str(vowel_count))
 
+words = []
+words_dict = {}
 
-words=[]
-words_dict={}
+exp = re.sub("[.,']", "", text)
 
-exp = re.sub("[.,']", "" , text)
-
+# Checking if the word is already in the dictionary else adding it to the dictionary
+# and counting number of times the word occurred in the text 
 for i in exp.split():
     if i == "":
         pass
+    
     else:
         words.append(i)
 
-    words_dict[i]=words.count(i)
+    words_dict[i] = words.count(i)
 
 print(words_dict)
