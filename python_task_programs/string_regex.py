ef406085ac267b65cb5007a129d28a32c21b87c0
SivWebProjects <99475439+SivWebProjects@users.noreply.github.com>
2022-04-12 11:07:06 +0530

    Update string_regex.py

--- python_task_programs/string_regex.py
+++ python_task_programs/string_regex.py
@@ -1,12 +1,15 @@
-#"This#string%contains^special*characters&.";   
-#I want string without all special characters from above string
+# "This#string%contains^special*characters&.";   
+# Print string without all special characters from above string.
 
 import re
 
 string = input("Enter the string: ")
+
+# Replacing special characters with space in the given string
 string_without_special_chars = re.sub("[^a-zA-Z0-9]", " " , string)
 
-# count the number of special characters in the given string
+# Count the number of special characters in the given string
 count_of_special_chars = string_without_special_chars.count(" ")
 
-print("String without all special characters and number of special characters in the given string are: " + string_without_special_chars + " and " + str(count_of_special_chars))
+print("String without all special characters in the given string is: " + string_without_special_chars)
+print("Count of special characters in the given string is: " + str(count_of_special_chars))
