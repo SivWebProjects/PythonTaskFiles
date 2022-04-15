23f22570691062f66c836d4c57bd945cf23cf877
SivWebProjects <99475439+SivWebProjects@users.noreply.github.com>
2022-04-15 13:39:23 +0530

    added comments

--- python_task_programs/string_regex.py
+++ python_task_programs/string_regex.py
@@ -6,7 +6,7 @@ import re
 string = input("Enter the string: ")
 
 # Replacing special characters with space in the given string
-string_without_special_chars = re.sub("[^a-zA-Z0-9]", " " , string)
+string_without_special_chars = re.sub("[^a-zA-Z0-9]", " ", string)
 
 # Count the number of special characters in the given string
 count_of_special_chars = string_without_special_chars.count(" ")
