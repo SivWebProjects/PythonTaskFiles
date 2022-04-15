a1dec1e38fe0983ee57817a5c537332e8c5eebf9
SivWebProjects <99475439+SivWebProjects@users.noreply.github.com>
2022-04-15 15:33:57 +0530

    adding comments and docstrings

--- python_task_programs/find_majority.py
+++ python_task_programs/find_majority.py
@@ -7,27 +7,31 @@ majority_vote(["A", "A", "A", "B", "C", "A"]) ➞ "A"
 majority_vote(["A", "B", "B", "A", "C", "C"]) ➞ None
 """ 
 
+
 def finding_majority(lst):
-    """Returns the majority elements in a list
-    that occurs greater than half of the length of the list times
+    """
+    Returns the majority elements in a list
+    that occurs greater than half of the length of the list times.
     
     Parameters:
-        lst: list
+        lst (list): A list consists of space separated strings
+        
     Returns:
-        majority_list: list item
+        majority_list (str): A string
     """
     unique_ele = list(set(lst))
     majority_list = [item for item in unique_ele if lst.count(item) > (len(lst) // 2)]
+    
     return majority_list
 
 
-lst = input().split()
-#print(lst)
+lst = input('Enter space separated strings: ').split()
 
-#Calling the function and passing given list as an argument
-majority_list = finding_majority(lst)
+# Calling the function and passing given list as an argument
+result = finding_majority(lst)
 
-if len(majority_list) == 0:
+if len(result) == 0:
     print("Majority Vote in the given list is None.")
+    
 else:
-    print("Majority Vote in the given list is " + " ".join(majority_list) + ".")
+    print("Majority Vote in the given list is " + str(result[0]) + ".")
