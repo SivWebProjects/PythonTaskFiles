8a2458c2cff6c94014102f30cf5bd657a3f96d14
SivWebProjects <99475439+SivWebProjects@users.noreply.github.com>
2022-04-15 14:48:57 +0530

    added comments and docstring

--- python_task_programs/disarium_number.py
+++ python_task_programs/disarium_number.py
@@ -1,9 +1,16 @@
 # Create a function that determines whether a number is a Disarium or not.
 
 def finding_disarium(num):
-    """takes number as an input then sum of its digits powered with their respective position 
-    and print disarium number when the given number is equal to the original number 
-    else it print not a disarium number.
+    """
+    Takes number as an input then sum of its digits powered with their respective position
+    and print a disarium number when the given number is equal to the original number 
+    else it prints not a disarium number.
+
+    Parameters:
+        num (int): An integer
+
+    Returns:
+        result (str): A string whether it is a disarium or not
     """
     list_of_digits = list(num)
     length = len(list_of_digits)
@@ -12,14 +19,18 @@ def finding_disarium(num):
     for i in range(length):
         sum += int(list_of_digits[i]) ** (i+1)
     
+    result = ""
     if sum == int(num):
-        print("Given number " + num + " is a Disarium Number.")  
-        
+        result = "Given number " + num + " is a Disarium Number."
+
     else:
-        print("Given number " + num + " is not a Disarium Number.")
+        result = "Given number " + num + " is not a Disarium Number."
+
+    return result
 
 
 number = input("Enter a number: ")
 
 # Calling the function by passing number as an argument
-finding_disarium(number)
+result = finding_disarium(number)
+print(result)
