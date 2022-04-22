65cf5ceb73b1c269ccd33bd577db7099f7e95742
SivWebProjects <99475439+SivWebProjects@users.noreply.github.com>
2022-04-22 11:18:53 +0530

    displays html content in web page

---
+++ flask_practice/displaying_html.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('dis_html_home.html', content="Flask")
+
+
+@app.route('/index')
+def index():
+    return render_template('dis_html_index.html', content=['HTML', 'CSS', 'JavaScript'])
+
+
+app.run(debug=True)
