e43a50a3e2da573cbb58da3a7f32bff9f35b726a
SivWebProjects <99475439+SivWebProjects@users.noreply.github.com>
2022-05-13 10:48:19 +0530

    added comments

--- python_task_programs/reading_csv_file.py
+++ python_task_programs/reading_csv_file.py
@@ -7,7 +7,7 @@ import csv
 # Open the text file in read mode
 file = open("/home/neosoft/Documents/PythonFiles/venv/name_data.csv", "r")
 
-# Creating a update_delete_ajax_jquery csv file
+# Creating a new csv file
 new_file = open("name_data_copy.csv", 'x')
 
 data = csv.reader(file)
@@ -24,7 +24,7 @@ print("Original Data in CSV File: " + str(data_list))
 length = len(data_list)
 print("Length of list is: " + str(length))
 
-# Writes all the rows except last two rows into the update_delete_ajax_jquery file
+# Writes all the rows except last two rows into the new file
 for i in range(length - 2):
     writer.writerow(data_list[i])
 
