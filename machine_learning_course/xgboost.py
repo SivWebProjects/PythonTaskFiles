36f9a9317963c33c4a9d9fb7b9671a3a8d4c7e0c
SivWebProjects <99475439+SivWebProjects@users.noreply.github.com>
2022-05-22 23:55:40 +0530

    xgboost

--- machine_learning_course/xgboost.py
+++ machine_learning_course/xgboost.py
@@ -1,7 +1,10 @@
-# Importing the libraries
+# Importing required libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.model_selection import train_test_split, cross_val_score
+from xgboost import XGBClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score
 
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
@@ -9,24 +12,20 @@ x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
 # Training XGBoost on the Training set
-from xgboost import XGBClassifier
 classifier = XGBClassifier()
 classifier.fit(x_train, y_train)
 
 # Confusion Matrix
-from sklearn.metrics import confusion_matrix, accuracy_score
 y_pred = classifier.predict(x_test)
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
 accuracy_score(y_test, y_pred)
 
 # Applying k-Fold Cross Validation
-from sklearn.model_selection import cross_val_score
-accuracies = cross_val_score(estimator = classifier, X = x_train, y = y_train, cv = 10)
+accuracies = cross_val_score(estimator=classifier, X=x_train, y=y_train, cv=10)
 print(accuracies)
-print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
-print("Standard Deviation: {:.2f} %".format(accuracies.std()*100))
+print("Accuracy: {:.2f} %".format(accuracies.mean() * 100))
+print("Standard Deviation: {:.2f} %".format(accuracies.std() * 100))
