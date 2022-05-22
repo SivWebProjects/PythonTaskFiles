b1223775751cf8d9d6ae77b996d3d29d7c5031b8
SivWebProjects <99475439+SivWebProjects@users.noreply.github.com>
2022-05-23 00:05:49 +0530

    detection of people who wear masks incorrectly, improperly, or not using cnn

--- detect_mask_cnn.py
+++ detect_mask_cnn.py
@@ -1,23 +1,27 @@
-# Importing the libraries
+# Importing necessary libraries
 import tensorflow as tf
-from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing import image
+# from keras.preprocessing.image import ImageDataGenerator
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
 
 # Step 1 - Data Preprocessing
 # a) Preprocessing the Training set
-train_datagen = ImageDataGenerator(rescale = 1./255,
-                                   shear_range = 0.2,
-                                   zoom_range = 0.2,
-                                   horizontal_flip = True)
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True)
 training_set = train_datagen.flow_from_directory('/home/neosoft/Documents/Github/PythonTaskFiles/cnn_mask_detection/mask_dataset/train',
-                                                 target_size = (64, 64),
-                                                 batch_size = 32,
-                                                 class_mode = 'binary')
+                                                 target_size=(64, 64),
+                                                 batch_size=32,
+                                                 class_mode='binary')
 # b) Preprocessing the Test set
-test_datagen = ImageDataGenerator(rescale = 1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
 test_set = test_datagen.flow_from_directory('/home/neosoft/Documents/Github/PythonTaskFiles/cnn_mask_detection/mask_dataset/test',
-                                            target_size = (64, 64),
-                                            batch_size = 32,
-                                            class_mode = 'binary')
+                                            target_size=(64, 64),
+                                            batch_size=32,
+                                            class_mode='binary')
 
 # Step 2 - Building the CNN
 cnn = tf.keras.models.Sequential()
@@ -37,31 +41,26 @@ cnn.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
 
 # Step 3 - Training the CNN
 # Compiling the CNN
-cnn.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+cnn.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 # Training the CNN on the Training set and evaluating it on the Test set
-cnn.fit(x = training_set, validation_data = test_set, epochs = 25)
+cnn.fit(x=training_set, validation_data=test_set, epochs=25)
 
 # Step 4 - Making a single prediction
-import cv2
-from matplotlib import pyplot as plt
-import numpy as np
-from keras.preprocessing import image
-
-# Reading image using opencv imread() 
+# Reading image using opencv imread()
 image_path = "/home/neosoft/Documents/Github/PythonTaskFiles/cnn_mask_detection/mask_dataset/images.jpeg"
 predict_image = cv2.imread(image_path)
 predict_image = cv2.cvtColor(predict_image, cv2.COLOR_BGR2RGB)
 plt.imshow(predict_image)
 
-predict_image = image.load_img(image_path, target_size = (64, 64))
+predict_image = image.load_img(image_path, target_size=(64, 64))
 predict_image = image.img_to_array(predict_image)
-predict_image = np.expand_dims(predict_image, axis = 0)
+predict_image = np.expand_dims(predict_image, axis=0)
 result = cnn.predict(predict_image)
-# show indices of classes
-training_set.class_indices
+# shown indices of classes
+print(training_set.class_indices)
 if result[0][0] == 1:
-  prediction_class = 'Properly worn mask'
+    prediction_class = 'Properly worn mask'
 else:
-  prediction_class = 'Improperly worn mask'
+    prediction_class = 'Improperly worn mask'
 
 print(prediction_class)
