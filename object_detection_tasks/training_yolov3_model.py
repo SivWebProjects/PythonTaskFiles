64c44619700f6934728c4366db0c032725c3d593
SivWebProjects <99475439+SivWebProjects@users.noreply.github.com>
2022-05-23 00:13:16 +0530

    training yolov3 for object detection with custom data

--- object_detection_tasks/training_yolov3_model.py
+++ object_detection_tasks/training_yolov3_model.py
@@ -3,9 +3,10 @@ from google.colab import drive
 drive.mount('/content/drive')
 
 # Unzipping the zip file
-# !unzip '/content/drive/MyDrive/yolo_custom_model_training/custom_dataset.zip' 
-  -d '/content/drive/MyDrive/yolo_custom_model_training/'
+# !unzip '/content/drive/MyDrive/yolo_custom_model_training/custom_dataset.zip'
+# -d '/content/drive/MyDrive/yolo_custom_model_training/'
 
+# Clone the darknet
 # !git clone 'https://github.com/AlexeyAB/darknet.git' '/content/drive/MyDrive/yolo_custom_model_training/darknet'
 
 # Changing the directory to the darknet
@@ -13,7 +14,7 @@ drive.mount('/content/drive')
 
 # running make file
 # !make
-# While running the make file multiple times, a permission denied error will occur. 
+# While running the make file multiple times, a permission denied error will occur.
 # To resolve the above error, the following four lines should be run.
 %cd darknet
 !sed -i 's/GPU=0/GPU=1/g' Makefile
@@ -31,6 +32,6 @@ drive.mount('/content/drive')
 # for training in Darknet framework
 !python custom_dataset/creating-train-and-test-txt-files.py
 
-# Training the model   
-!darknet/darknet detector train custom_dataset/labelled_data.data darknet/cfg/yolov3_custom.cfg 
+# Training the model ans saving the weights
+!darknet/darknet detector train custom_dataset/labelled_data.data darknet/cfg/yolov3_custom.cfg
 custom_weight/darknet53.conv.74 -dont_show
