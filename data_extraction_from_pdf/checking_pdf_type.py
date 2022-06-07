8d93956872080c31a5e15295197af598beb43988
SivWebProjects <99475439+SivWebProjects@users.noreply.github.com>
2022-06-07 10:18:54 +0530

    checking pdf type

--- data_extraction_from_pdf/checking_pdf_type.py
+++ data_extraction_from_pdf/checking_pdf_type.py
@@ -5,7 +5,7 @@ pdf_files = ["/home/neosoft/Documents/recipe_data/crockpot_recipes.pdf",
              "/home/neosoft/Documents/recipe_data/barbecue_recipes.pdf"]
 
 
-def pdf_type(fname):
+def pdf_type(pdf_file_path):
     """
     It determines whether the pdf is electronic or not based on the font property.
     :param fname: The location of the pdf file
@@ -14,7 +14,7 @@ def pdf_type(fname):
     e_pages = []
     image_pages = []
     page_num = 0
-    with open(fname, 'rb') as infile:
+    with open(pdf_file_path, 'rb') as infile:
         for page in PDFPage.get_pages(infile):
             page_num += 1
             if 'Font' in page.resources.keys():
@@ -27,3 +27,9 @@ def pdf_type(fname):
         return "Image PDF"
     else:
         return "Not a valid document"
+
+      
+for pdf_file_path in pdf_files:
+    result = pdf_type(pdf_file_path)
+    print(result)
+    
